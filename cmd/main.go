bb053f2002abfc7b007948abe4bfdc4025d3f5f2
onceup <chistoyo@gmail.com>
2024-07-11 22:24:25 +0200

    feat: initial client commit

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	desc "github.com/onceup/go-grpc-ms/pkg/user_v1"
+)
+
+const (
+	address = "localhost:50051"
+	userId  = 12
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to server: %v", err)
+	}
+	defer conn.Close()
+
+	c := desc.NewUserV1Client(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.Get(ctx, &desc.GetRequest{Id: userId})
+	if err != nil {
+		log.Fatalf("failed to get user by id: %v", err)
+	}
+
+	log.Printf(color.RedString("User info:\n"), color.GreenString("%+v", r.GetUser()))
+}
